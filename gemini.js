c0a45f881733074e5b8877ee4d2e68519abcb467
Légrády Máté <legrady.mate77@gmail.com>
2026-07-24 14:34:01 +0200

    80 százalék

--- gemini.js
+++ gemini.js
@@ -714,7 +714,7 @@ const visibilityObserver = new IntersectionObserver(
             }
         });
     },
-    { threshold: 0.5 } // már akkor is "látható", ha csak 1%-a látszik
+    { threshold: 0.8 } // már akkor is "látható", ha csak 1%-a látszik
 );
 
 window.addEventListener("pagehide", stopAnimation);
